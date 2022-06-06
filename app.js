ff75ccae435d71ae8602aa38586f98550c4063b0
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2022-06-07 07:46:53 +0800

    module.exports & require() in Node.js

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * @author Kurt Russelle Marmol
+ * @param { please use only one function and log, uncomment other function}
+ */
+
+
+// 1. Normal Require
+// const converter = require('./library.js');
+
+// console.log(converter.celsiusToFahrenheit(20));
+
+
+// 2. Object Destructing 
+// const { converter } = require('./library.js');
+
+// console.log(celsiusToFahrenheit(20));
---
+++ library.js
@@ -0,0 +1,30 @@
+/**
+ * Different Module Exports and Require
+ * @author {Kurt Russelle Marmol}
+ * @param { github.com/jkrmarmol }
+ * @param { please use only one function, uncomment other function }
+ */
+
+
+// First 
+// module.exports.celsiusToFahrenheit = function celsiusToFahrenheit(val) {
+//     return (val * 9/5) + 32;
+// }
+
+
+// Second
+// function celsiusToFahrenheit(val) {
+//     return (val * 9/5) + 32;
+// }
+
+// module.exports.celsiusToFahrenheit = celsiusToFahrenheit;
+
+
+// Third (Object Destructuring to be more Selective With require()) 
+// function celsiusToFahrenheit(val) {
+//     return (val * 9/5) + 32;
+// }
+
+// module.exports = { celsiusToFahrenheit };
+
+
